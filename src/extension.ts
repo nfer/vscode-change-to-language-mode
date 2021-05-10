77e48597b4f794b0917e909a64c7e5f947ad5685
Nfer Zhuang <nfer.zhuang@gmail.com>
2021-05-10 17:53:49 +0800

    add more commands

--- src/extension.ts
+++ src/extension.ts
@@ -1,16 +1,24 @@
 import * as vscode from 'vscode';
 
 export function activate(context: vscode.ExtensionContext) {
-	let disposable = vscode.commands.registerCommand('change-to-language-mode.changToHTML', () => {
-		const activeEditor = vscode.window.activeTextEditor;
-		if (!activeEditor) {
-			return;
-		}
+	function changeTo(cmd: string, language: string) {
+		const disposable = vscode.commands.registerCommand(`change-to-language-mode.${cmd}`, () => {
+			const activeEditor = vscode.window.activeTextEditor;
+			if (!activeEditor) {
+				return;
+			}
+	
+			vscode.languages.setTextDocumentLanguage(activeEditor.document, language);
+		});
+	
+		context.subscriptions.push(disposable);
+	}
 
-		vscode.languages.setTextDocumentLanguage(activeEditor.document, 'html');
-	});
-
-	context.subscriptions.push(disposable);
+	changeTo('changeToHTML', 'html');
+	changeTo('changeToJavaScript', 'javascript');
+	changeTo('changeToCSS', 'css');
+	changeTo('changeToJSON', 'json');
+	changeTo('changeToTypeScript', 'typescript');
 }
 
 export function deactivate() {}
